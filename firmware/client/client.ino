6c6c00afcc84893dcd65ec6990636cde679636f9
tomoro1729-a <239909458+tomoro1729-a@users.noreply.github.com>
2026-05-27 14:09:35 +0900

    輪唱制御

---
+++ firmware/client/client.ino
@@ -0,0 +1,51 @@
+// クライアント機 輪唱演奏システム メインプログラム（
+#include "config.h"
+
+bool isPlaying = false;
+int  beatCountdown = -1;  
+
+int lastSyncState = LOW;
+
+void setup() {
+    Serial.begin(SERIAL_BAUD);
+    pinMode(PIN_SYNC_IN, INPUT);
+    pinMode(PIN_LED, OUTPUT);
+    digitalWrite(PIN_LED, LOW);
+    beatCountdown = ROUND_DELAY_BEATS;
+    Serial.print("CLIENT_ID=");   Serial.print(CLIENT_ID);
+    Serial.print(" DELAY_BEATS="); Serial.println(ROUND_DELAY_BEATS);
+}
+
+void loop() {
+
+    int currentSyncState = digitalRead(PIN_SYNC_IN);
+
+    if (currentSyncState == HIGH && lastSyncState == LOW) {
+
+        if (!isPlaying) {
+            if (beatCountdown > 0) {
+
+                beatCountdown--;
+            } else if (beatCountdown == 0) {
+  
+                isPlaying = true;
+                beatCountdown = -1; 
+                onBeat();
+            }
+        } 
+ 
+        else {
+            onBeat();
+        }
+    }
+    lastSyncState = currentSyncState;
+}
+
+
+void onBeat() {
+    digitalWrite(PIN_LED, HIGH);
+    delay(20); 
+    digitalWrite(PIN_LED, LOW);
+
+    Serial.write(0x03); 
+}
