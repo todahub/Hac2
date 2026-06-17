93661f168d269c5ceb3ccb3446e705e175d3dd65
tomoro1729-a <239909458+tomoro1729-a@users.noreply.github.com>
2026-06-17 14:49:23 +0900

    テスト

--- firmware/client/value_com/value.ino
+++ firmware/client/value_com/value.ino
@@ -4,13 +4,15 @@ const int ID = 1;
 bool active = false;
 
 const int NOTE_COUNT = 37;
-float frequencies[NOTE_COUNT] = {
-  261.63, 293.66, 329.63, 349.23, 329.63, 293.66, 261.63, 0.0,
-  329.63, 349.23, 392.00, 440.00, 392.00, 349.23, 329.63, 0.0,
-  261.63, 0.0,    261.63, 0.0,    261.63, 0.0,    261.63, 0.0,
-  261.63, 261.63, 293.66, 293.66, 329.63, 329.63, 349.23, 349.23,
-  329.63, 0.0,    293.66, 0.0,    261.63
+
+const char* noteNames[NOTE_COUNT] = {
+  "C4", "D4", "E4", "F4", "E4", "D4", "C4", "C4",
+  "E4", "F4", "G4", "A4", "G4", "F4", "E4", "E4",
+  "C4", "C4", "C4", "C4", "C4", "C4", "C4", "C4",
+  "C4", "C4", "D4", "D4", "E4", "E4", "F4", "F4",
+  "E4", "E4", "D4", "D4", "C4"
 };
+
 float beats[NOTE_COUNT] = {
   1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
   1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
@@ -27,11 +29,16 @@ float velocities[NOTE_COUNT] = {
 };
 
 int currentNoteIndex = 0;
+int lastSentIndex = -1;
 unsigned long noteStartTime = 0;
 float elapsedBeats = 0.0;
 
 unsigned long triggerPulseStartMs = 0;
 bool triggerPulseActive = false;
+
+float currentBeatLengthMs = 0.0;
+
+void sendNoteData(int index);
 
 void setup() {
   Serial.begin(115200);
@@ -43,6 +50,7 @@ void setup() {
 void loop() {
   int bpmValue = analogRead(A1);
   float bpm = 60.0 + ((float)bpmValue * (180.0 / 1023.0));
+  currentBeatLengthMs = (60.0 / bpm) * 1000.0;
 
   int sensorValue = analogRead(A0);
   bool inRange = abs(sensorValue - (ID * 205)) < 102;
@@ -51,9 +59,9 @@ void loop() {
     active = true;
     digitalWrite(13, HIGH);
     currentNoteIndex = 0;
+    lastSentIndex = -1;
     elapsedBeats = 0.0;
     noteStartTime = millis();
-    sendNoteData(currentNoteIndex);
   }
 
   if (active) {
@@ -61,7 +69,6 @@ void loop() {
     unsigned long duration = now - noteStartTime;
     noteStartTime = now;
 
-    float currentBeatLengthMs = (60.0 / bpm) * 1000.0;
     elapsedBeats += (float)duration / currentBeatLengthMs;
 
     if (elapsedBeats >= beats[currentNoteIndex]) {
@@ -74,14 +81,17 @@ void loop() {
         triggerPulseStartMs = millis();
         triggerPulseActive = true;
       }
+    }
 
-      if (currentNoteIndex < NOTE_COUNT) {
+    if (currentNoteIndex < NOTE_COUNT) {
+      if (currentNoteIndex != lastSentIndex) {
         sendNoteData(currentNoteIndex);
-      } else {
-        active = false;
-        digitalWrite(13, LOW);
-        Serial.println("0,0,0");
+        lastSentIndex = currentNoteIndex;
       }
+    } else {
+      active = false;
+      digitalWrite(13, LOW);
+      Serial.println("END,0,0");
     }
   }
 
@@ -92,14 +102,11 @@ void loop() {
 }
 
 void sendNoteData(int index) {
-  Serial.print(frequencies[index], 2);
+  Serial.print(noteNames[index]);
   Serial.print(",");
   
-  if (beats[index] == (int)beats[index]) {
-    Serial.print(beats[index], 0);
-  } else {
-    Serial.print(beats[index], 1);
-  }
+  float durationMs = beats[index] * currentBeatLengthMs;
+  Serial.print(durationMs, 0);
   
   Serial.print(",");
   Serial.println(velocities[index], 0);
